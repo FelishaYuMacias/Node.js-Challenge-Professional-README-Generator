a888c907ef96ad62b07fee74f9dfb53b02f08020
Felisha <felisha.j.yu@gmail.com>
2022-10-20 16:24:24 -0700

    updated comments added more text to README

--- assets/js/index.js
+++ assets/js/index.js
@@ -50,7 +50,7 @@ Or check out their [Github](https://github.com/${github})
 
 ## License
 
-${license}
+${license} - See repo for detailed information.
 
 `
 
@@ -124,24 +124,13 @@ const startPrompt = () => {
 })  }
 
 startPrompt();
-    //TODO:WHEN I enter my project title
-    //THEN this is displayed as the title of the README
-    //TODO:WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
-        //THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+   
+   
     //TODO:WHEN I choose a license for my application from a list of options
         //THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-    //TODO:WHEN I enter my GitHub username
-        //THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-    //TODO:WHEN I enter my email address
-        //THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-    //TODO:WHEN I click on the links in the Table of Contents
-        //THEN I am taken to the corresponding section of the README
-//The application will be invoked by using the following command: node index.js
+    
 
-
-//TODO:WHEN I am prompted for information about my application repository
-    //THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-//provide a link to a walkthrough video that demonstrates its functionality.
+//TODO: provide a link to a walkthrough video that demonstrates its functionality.
     //Revisit the Screencastify Tutorial in the prework as a refresher on how to record video from your computer.
     //You’ll need to submit a link to the video _and_ add it to the README of your project.
 
